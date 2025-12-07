2e5e7dab3007aaacf15b2ed5a3e8d7f2f794587a
Stuti <stutipatel7667@gmail.com>
2025-12-06 23:27:23 -0500

    feat: add auth controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+exports.register = (req, res) => {
+    res.json({ message: "Register API working" });
+  };
+  
+  exports.login = (req, res) => {
+    res.json({ message: "Login API working" });
+  };
+  

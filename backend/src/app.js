b85adcc62dce9b0af7e2362508ae8286ab5ab415
Stuti <stutipatel7667@gmail.com>
2025-12-07 00:26:00 -0500

    feat: add user registration API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
+const authRoutes = require("./routes/auth.routes");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");

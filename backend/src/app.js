dad64b3313bcdc8c65f8a32c54124f85ecfa05b8
Stuti <stutipatel7667@gmail.com>
2025-12-06 23:29:28 -0500

    feat: add auth routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/api/auth");
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 

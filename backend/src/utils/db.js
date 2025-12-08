96a6e3b063e1890c4b102b4848bccdd8a40883d8
Stuti <stutipatel7667@gmail.com>
2025-12-07 22:52:40 -0500

    updated db.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO URI BEING USED:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
-  } catch (err) {
-    console.error("DB connection error:", err);
+  } catch (error) {
+    console.error("DB connection error:", error);
   }
 };
 

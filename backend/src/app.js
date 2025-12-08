ddd24d369dd42db8a170d9e5caaf1e491d3635cf
Stuti <stutipatel7667@gmail.com>
2025-12-07 23:14:58 -0500

    feat: connect auth routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,9 @@
 const authRoutes = require("./routes/auth.routes");
 const express = require("express");
+
 const app = express();
-
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
-
-
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
 
 module.exports = app;

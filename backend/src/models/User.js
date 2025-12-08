a35ba8f898991b23ee6bf65b22d854ed84290074
Stuti <stutipatel7667@gmail.com>
2025-12-07 23:11:49 -0500

    feat: add user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: String,
+  email: String,
+  password: String,
 });
 
 module.exports = mongoose.model("User", UserSchema);

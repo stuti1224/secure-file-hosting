401ddc5eb9d2e332577b4cbc662b39ba5c583988
Stuti <stutipatel7667@gmail.com>
2025-12-07 23:38:14 -0500

    connecting file routes after fixing the routes files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const authRoutes = require("./routes/auth.routes");
 const express = require("express");
+const fileRoutes = require("./routes/fileRoutes");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
 
 module.exports = app;

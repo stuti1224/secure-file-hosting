78b3f8b98979ecd44536faa1ce5db74a26b19fcd
Stuti <stutipatel7667@gmail.com>
2025-12-10 17:07:28 -0500

    update

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../controllers/auth.controller");
+const { register, login } = require("../controllers/auth.controller");
 
-router.post("/register", auth.register);
-router.post("/login", auth.login);
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;

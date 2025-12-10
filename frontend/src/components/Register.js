1612ec46a6c8b89a13716c9bdfd86857570cde54
Stuti <stutipatel7667@gmail.com>
2025-12-10 16:12:56 -0500

    fixed to connect and check register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,41 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../api/authApi";
+
 export default function Register() {
-    return <h1>Register Page Test</h1>;
-  }
-  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await registerUser({ name, email, password });
+      alert(response.message); // show success message
+      navigate("/"); // redirect to login
+    } catch (err) {
+      setError(err.response?.data?.message || "Registration failed");
+    }
+  };
+
+  return (
+    <div style={{ padding: "100px" }}>
+      <h1>REGISTER PAGE</h1>
+      <form onSubmit={handleRegister}>
+        <input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} /><br/><br/>
+        <input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br/><br/>
+        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br/><br/>
+        <button type="submit">register</button>
+      </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <br/>
+      <Link to="/">go to login</Link>
+    </div>
+  );
+}
